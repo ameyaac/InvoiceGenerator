98625160eda807e5a5bcbcfcc6ee0a64f167fccf
Ameyaa <ameyaa.c12@gmail.com>
2025-03-28 00:33:33 +0530

    Added particulars component and edited the header a little

---
+++ src/app/particulars/particulars.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-particulars',
+  standalone: true,
+  imports: [],
+  templateUrl: './particulars.component.html',
+  styleUrl: './particulars.component.scss'
+})
+export class ParticularsComponent {
+
+}
