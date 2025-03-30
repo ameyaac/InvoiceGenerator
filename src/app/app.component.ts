de8a6ea100c44d787a919b064c53d703909a3b70
Ameyaa <ameyaa.c12@gmail.com>
2025-03-30 14:40:18 +0530

    Added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ParticularsComponent } from "./particulars/particulars.component";
+import { Title } from '@angular/platform-browser';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ParticularsComponent],
+  imports: [RouterOutlet, HeaderComponent, ParticularsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'InvoiceGenerator';
+  constructor(private title: Title) {
+    title.setTitle("🧾 Invoice Generator")
+  }
 }
